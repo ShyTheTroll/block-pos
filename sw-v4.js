24a00976b5c545bb550a03989cc82f2cc4c25765
ShyTheTroll <122953543+ShyTheTroll@users.noreply.github.com>
2025-08-21 05:39:50 +0800

    Add files via upload

---
+++ sw-v4.js
@@ -0,0 +1,12 @@
+const CACHE = 'block-pos-mobile-profit-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
